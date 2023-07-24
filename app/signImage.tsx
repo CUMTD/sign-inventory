e2b91888d166190fc7a1cb8e0f83590ea301dee7
Matthew Novelli <mattnovelli@gmail.com>
2023-07-24 14:08:05 -0500

    new logo

--- app/signImage.tsx
+++ app/signImage.tsx
@@ -2,15 +2,19 @@
 
 import image from '@public/bus_stop_sign.png';
 import Image from 'next/image';
+import styles from './searchBox.module.css';
 
 export default function SignImage() {
-	return <Image
-		src={image}
-		alt="logo"
-		width={139}
-		height={200}
-		title="it's a great day to ride MTD!"
-		placeholder="blur"
-		priority
-	/>
+	return (
+		<Image
+			src={image}
+			className={styles.signImage}
+			alt="logo"
+			width={139}
+			height={200}
+			title="it's a great day to ride MTD!"
+			placeholder="blur"
+			priority
+		/>
+	);
 }
