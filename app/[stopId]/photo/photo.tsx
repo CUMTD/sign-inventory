ba66487c868740e1b851685a2f11c801268ff3ea
Matthew Novelli <mattnovelli@gmail.com>
2023-08-08 08:45:42 -0500

    improved photo display logic

--- app/[stopId]/photo/photo.tsx
+++ app/[stopId]/photo/photo.tsx
@@ -5,10 +5,12 @@ import { Stack } from '@mui/material';
 import styles from '../page.module.css';
 import { useRecoilValue } from 'recoil';
 import { selectedStopIdSelector } from '@state/serverDataState';
+import { useState } from 'react';
 const ENDPOINT = process.env.NEXT_PUBLIC_INVENTORY_API_ENDPOINT;
 
 export default function PhotoPage() {
 	const fullStopId = useRecoilValue(selectedStopIdSelector);
+	const [hideImage, setHideImage] = useState(false);
 
 	return (
 		<>
@@ -16,14 +18,15 @@ export default function PhotoPage() {
 				<div className={styles.subSection}>
 					<label>Upload a new photo:</label>
 					<input type="file" accept="image/jpeg" />
-					<div style={{ position: 'relative', minWidth: '50vw', minHeight: '50vh' }}>
+					<div
+						style={{ position: 'relative', minHeight: '30em', minWidth: '50em', display: hideImage ? 'none' : 'block' }}
+					>
 						<Image
 							src={`${ENDPOINT}/child-stop/${fullStopId}`}
 							fill={true}
-							alt="No image yet!"
-							// width={719}
-							// height={1279}
-							style={{ objectFit: 'contain' }}
+							alt={`Image for ${fullStopId}`}
+							className={styles.image}
+							onError={() => setHideImage(true)}
 						/>
 					</div>
 				</div>
